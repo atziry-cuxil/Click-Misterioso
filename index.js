697180a6f7bb4fbf783f32b55cdcfc9b66102561
Atziry Cuxil <cuxilatziry@gmail.com>
2026-05-28 14:55:39 -0600

    Agrego cambios en el contador y los botones

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let intentos = document.querySelector('#intentos')
 let resultado = Math.floor(Math.random() * botones.length);
 let contador = 0;
 let indices = [];
+let verificarFin = false;
 
 reiniciar.addEventListener('click', (event) => {
     botones[resultado].textContent = ''
@@ -12,24 +13,27 @@ reiniciar.addEventListener('click', (event) => {
     intentos.textContent = 'Intentos: '
     resultado = Math.floor(Math.random() * botones.length);
     indices = [];
+    verificarFin = false;
 })
 
 console.log(resultado)
 botones.forEach((btn, indice) => {
     btn.addEventListener('click', (event) => {
 
-        if (!indices.includes(indice)){
-            indices.push(indice)
-            contador++            
-        }
-
-        event.target.className = 'btn btn-warning p2'
-        intentos.textContent = 'Intentos: ' + contador
-
         if (indice == resultado) {
             event.target.textContent = "Me encontraste"
             alert('Felicidades!!')
             intentos.textContent = 'Intentos: ' + contador
+            verificarFin = true;
+        } else {
+            if (!verificarFin) {
+                if (!indices.includes(indice)) {
+                    indices.push(indice)
+                    contador++
+                }
+                event.target.className = 'btn btn-warning p2'
+                intentos.textContent = 'Intentos: ' + contador
+            }
         }
         
     })
